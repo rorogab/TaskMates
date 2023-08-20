200740d9897bde42031e71008f6675e2a9aca359
Paula Burghelea <paulaburghelea.github@gmail.com>
2023-08-20 18:49:08 +0100

    all bills displayed

--- client/src/components/Bills.jsx
+++ client/src/components/Bills.jsx
@@ -1,9 +1,47 @@
+import { useState, useEffect } from "react";
+
 import "./Bills.css";
 
 export default function Bills() {
+	const [bills, setBills] = useState([]);
+
+	useEffect(() => {
+		getBills()
+	}, []);
+
+	const getBills = () => {
+		fetch("/api/bills")
+			.then(response => response.json())
+			.then(bills => {
+				setBills(bills);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	};
+
 	return (
-		<div className="bills-main container-fluid">
-			<h3>All Bills</h3>
+		<div className="bills-main">
+			<div className="bills-header">
+				<div><a href="#"></a>DUE DATE</div>
+				<div><a href="#"></a>PAID DATE</div>
+				<div><a href="#"></a>CATEGORY</div>
+				<div><a href="#"></a>PROVIDER</div>
+				<div><a href="#"></a>AMOUNT</div>
+				<div><a href="#"></a>STATUS</div>
+			</div>
+			<ul className="bills-content">
+				{bills.map(bill => (
+					<li key = {bill.id}>
+						<div>{bill.due_date.split("T")[0]} </div>
+						<div>{bill.paid_date && bill.paid_date.split("T")[0]} </div>
+						<div>{bill.category} </div>
+						<div>{bill.provider} </div>
+						<div>{bill.amount} </div>
+						<div>{`${bill.status !== 0 ? "Paid" : "Unpaid"} `} </div>
+					</li>
+				))}
+			</ul>
 		</div>
 	)
 }
--- routes/bills.js
+++ routes/bills.js
@@ -4,26 +4,118 @@ const db = require("../model/helper");
 
 //helper functions
 
-//returns all the Bills in ascending order
-const getAllBills = (req, res) => {
-	db("SELECT * FROM bills ORDER BY date ASC;")
+//returns all the Bills in ascending order based on due_date
+const getAllBills = (req, res, key) => {
+	db(`SELECT * FROM bills ORDER BY ${key} ASC;`)
 		.then(results => {
-		console.log(results.data);
 		res.send(results.data);
 	})
 	.catch(err => res.status(500).send(err));
 };
 
+//returns a bills based on the key you are serching for, eg /bills/categorys
+const selectBillByKey = (req, res, key, value) => {
+	db(`SELECT * FROM bills WHERE ${key} = "${value}";`)
+		.then(results => {
+		res.send(results.data);
+	})
+	.catch(err => res.status(500).send(err));
+ }
+
+
 //CREATE
 
+router.post("/", async function(req, res, next) {
+	const {due_date, paid_date, category, provider, notes, amount, status} = req.body;
+	console.log(req.body);
+	try {
+		await db(`INSERT INTO bills (due_date, paid_date, category, provider, notes, amount, status) 
+		VALUES ('${due_date}', ${paid_date ? `"${paid_date}"` : null}, '${category}', '${provider}', '${notes}', '${amount}', 0);`);
+		getAllBills(req, res, "id");
+	} catch (error) {
+	  res.status(500).send({ error: error.message });
+	}
+ });
 
 //READ
 
-router.get('/', async (req, res) => {
-	getAllBills(req, res);
+router.get('/', function(req, res) {
+	getAllBills(req, res, "id");
 });
+
+/* GET bill by id */
+
+router.get('/:id', (req, res) => {
+	const key = "id";
+	const value = req.params.id;
+	selectBillByKey(req, res, key, value);
+ });
+
+/* GET bills by category */
+
+router.get('/category/:value', (req, res) => {
+	const key = "category";
+	const value = req.params.value;
+	selectBillByKey(req, res, key, value);
+ });
+
+/* GET all Paid OR Unpaid bills (UNPAID status = 0) */
+
+router.get('/status/:value', (req, res) => {
+	const key = "status";
+	const value = req.params.value;
+	selectBillByKey(req, res, key, value);
+ });
 
 //UPDATE
 
-//DETELE
+/* PUT - Update paid_date OR amount OR status*/
+// router.put('/:id', async function(req, res) {
+// 	const id = req.params.id;
+// 	const {due_date, paid_date, category, provider, notes, amount, status} = req.body;
+// 	try {
+// 		await db(`UPDATE bills SET
+// 			due_date = "${due_date}",
+// 			paid_date = "${paid_date}",
+// 			category = "${category}",
+// 			provider = "${provider}",
+// 			notes = "${notes}",
+// 			amount = ${amount},
+// 			status = ${status}
+// 			WHERE id = ${id};`
+// 		);
+// 		const result = await db(`SELECT * FROM bills WHERE id = ${id};`);
+// 		res.send(result.data);
+// 	} catch (error) {
+// 	res.status(500).send({error: error.message});
+// 	}
+// });
 
+//Set status to 1 (paid) for the bill
+router.put("/:id", async (req, res) => {
+	const id = req.params.id;
+	try {
+	  const result = await db(`UPDATE bills SET status = 1 WHERE id=${id};`);
+	  selectBillByKey(req, res, "id", id);
+	} catch (err) {
+	  res.status(500).send(err);
+	}
+});
+
+
+//DETELE
+router.delete('/:id', async function(req, res) {
+	const value = req.params.id;
+	try {
+		await db(`DELETE FROM bills WHERE id=${value};`);
+		getAllBills(req, res, "id")
+	} catch (error) {
+		res.status(500).send({ err: err.message });
+	}
+});
+
+module.exports = router;
+
+//Questions, 
+//Why have we decided on using TINYINT for Status and not Boolean?
+//Could Status be replaced by Paid? 
